32225afeda1a91ae13db8823cdae3c735bd687ab
Madelene Campos <madelenecampos@gmail.com>
2017-11-22 22:00:28 -0600

    Adds javascript

---
+++ public/javascripts/maddie.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var currentLocationEl = document.getElementById('location');
+
+// Images
+var images = [
+  {
+    'background-image': 'url(images/stars.gif)',
+    'background-size': 'cover'
+  },
+  { 'background-image': 'url(images/palm.jpg)' },
+  { 'background-image': 'url(images/polka.jpg)' }
+]
+
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(function() {
+  var el = document.querySelectorAll('.container')[0];
+  var attrs = images[Math.floor(Math.random() * images.length)];
+  Object.keys(attrs).forEach(function(key) {
+    var value = attrs[key];
+    el.style[key] = value;
+  })
+});
